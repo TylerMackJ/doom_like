fea19748379a356e610ba8da76aa6002a5bf0b9a
Tyler Johnson <tylermackj@gmail.com>
2021-07-19 18:59:16 -0500

    Changed from rustty to crossterminal

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-extern crate rustty;
 mod map;
 mod player;
 
+use std::io::{Write,
+    stdout
+};
+use crossterm::{
+    cursor,
+    queue,
+    execute,
+    style::{
+        SetBackgroundColor,
+        Color,
+        Print
+    },
+    terminal,
+    event::{
+        KeyCode,
+        KeyModifiers,
+        read,
+        poll,
+        Event
+    }
+};
 use map::Map;
 use player::Player;
-use rustty::{Terminal, Color, HasSize};
-use rustty::Event::Key;
-use std::time::{Duration, Instant};
+use std::time::{Duration,
+    Instant
+};
 use std::f64::consts::PI;
 
-fn main () {
-    let mut term = Terminal::new().unwrap();
+fn main() {
+    let mut stdout = stdout();
     let mut map = Map::new(15, 15, ' ');
-    let mut player = Player::new((7.0, 7.0), PI / 4.0, PI / 3.0, 8.0);
+    let mut player = Player::new((7.0, 7.0), PI / 4.0, PI / 3.0, 15.0);
 
     for x in 0..map.width {
         map[(x, 0)] = '#';
@@ -27,10 +47,12 @@ fn main () {
 
     map[(5, 5)] = '#';
 
+    execute!(stdout, terminal::EnterAlternateScreen, cursor::Hide).unwrap();
+    terminal::enable_raw_mode().unwrap();
+
     'game: loop {
         let start_time = Instant::now();
-
-        let screen_space = term.size();
+        let screen_space = terminal::size().unwrap();
 
         for x in 0..screen_space.0 {
             let mut ray: (f64, f64) = player.position();
@@ -40,22 +62,44 @@ fn main () {
                 ray.1 += angle.sin() * 0.1;
             }
             let ray_distance: f64 = ((ray.0 - player.position().0).powf(2.0) + (ray.1 - player.position().1).powf(2.0)).sqrt();
-            //println!("Ray hit {}, {} with distance {}", ray.0, ray.1, ray_distance);
 
             for y in 0..screen_space.1 {
                 angle = (player.vertical_fov() * (y as f64 / (screen_space.1 - 1) as f64)) - (player.vertical_fov() / 2.0);
                 let height: f64 = angle.sin() * ray_distance;
                 if height < 1.0 && height > -1.0 && ray_distance < player.view_distance() {
-
-                    term[(x, y)].set_bg(Color::Byte(((24.0 * (1.0 - (ray_distance / player.view_distance()))) as u8) + 232));
+                    let brightness_multiplier: f64 = 1.0 - (ray_distance / player.view_distance());
+                    queue!(stdout, cursor::MoveTo(x, y), SetBackgroundColor(Color::Rgb{r: (255.0 * brightness_multiplier) as u8, g: (128.0 * brightness_multiplier) as u8, b: (128.0 * brightness_multiplier) as u8}), Print(" ".to_string())).unwrap();
                 } else {
-                    term[(x, y)].set_bg(Color::Byte(232));
+                    queue!(stdout, cursor::MoveTo(x, y), SetBackgroundColor(Color::Rgb{r: 0, g: 0, b: 0}), Print(" ".to_string())).unwrap();
                 }
             }
         }
-        term.swap_buffers();
+        stdout.flush().unwrap();
 
-        while start_time.elapsed() < Duration::from_millis(16) {
+        let mut frame_duration = start_time.elapsed();
+
+        while frame_duration < Duration::from_millis(32) {
+            if poll(Duration::from_millis(32) - frame_duration).unwrap() {
+                match read().unwrap() {
+                    Event::Key(key_event) => {
+                        match key_event {
+                            e if e.code == KeyCode::Char('c') && e.modifiers == KeyModifiers::CONTROL => break 'game,
+                            e if e.code == KeyCode::Char('w') && e.modifiers == KeyModifiers::NONE    => player.move_camera_relative(( 0.05, 0.0)),
+                            e if e.code == KeyCode::Char('s') && e.modifiers == KeyModifiers::NONE    => player.move_camera_relative((-0.05, 0.0)),
+                            e if e.code == KeyCode::Char('a') && e.modifiers == KeyModifiers::NONE    => player.move_camera_relative(( 0.0, 0.05)),
+                            e if e.code == KeyCode::Char('d') && e.modifiers == KeyModifiers::NONE    => player.move_camera_relative(( 0.0,-0.05)),
+                            e if e.code == KeyCode::Left      && e.modifiers == KeyModifiers::NONE    => player.turn(-0.1),
+                            e if e.code == KeyCode::Right     && e.modifiers == KeyModifiers::NONE    => player.turn( 0.1),
+                            _ => {}
+                        }
+                    },
+                    _ => {}
+                }
+                frame_duration = start_time.elapsed()
+            }
+        }
+
+            /*
             let e = term.get_event(Duration::new(0, 0)).unwrap();
             match e {
                 Some(Key('\u{3}')) | Some(Key('\u{1a}')) | Some(Key('\u{11}'))  => break 'game,
@@ -81,6 +125,8 @@ fn main () {
                 }
                 _ => {},
             }
-        }
+            */
     }
+    terminal::disable_raw_mode().unwrap();
+    execute!(stdout, cursor::Show, terminal::LeaveAlternateScreen).unwrap();
 }
